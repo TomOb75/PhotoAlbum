dec85e88c29b31123d8fb1455e0f7fc07961850f
TomOb75 <tom.oblocki@cix.csi.cuny.edu>
2025-03-17 17:12:58 -0400

    added an imeaction to keyboard

--- app/src/main/java/com/example/photoalbum/MainActivity.kt
+++ app/src/main/java/com/example/photoalbum/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -97,7 +98,7 @@ fun SlideShow(modifier: Modifier = Modifier) {
             value = inputText,
             onValueChange = { inputText = it },
             label = { Text(stringResource(R.string.image_search)) },
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
             singleLine = true,
             modifier = Modifier.fillMaxWidth(0.8f)
         )
